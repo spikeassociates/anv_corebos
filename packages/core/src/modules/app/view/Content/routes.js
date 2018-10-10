8b4234fe8764ab155f8530452e0e4ec8c18ce6fb
Andi Ndrecka <andi.ndrecka1@gmail.com>
2018-10-10 15:17:17 +0200

    routes refactor

--- packages/core/src/modules/app/view/Content/routes.js
+++ packages/core/src/modules/app/view/Content/routes.js
@@ -1,20 +1,24 @@
-const subitoRoutes = [
-  { name: "master account", module: "listview", corebosModule: "MasterAccount" },
-  { name: "account", module: "listview", corebosModule: "Accounts" },
-  { name: "task", module: "listview", corebosModule: "Task" },
-  { name: "sales order", module: "listview", corebosModule: "SalesOrderMaster" },
-  { name: "cases", module: "listview", corebosModule: "Cases" },
-  { name: "account details", module: "listview", corebosModule: "AccountDetails" },
-  { name: "term", module: "listview", corebosModule: "Term" },
-  { name: "adoc master", module: "listview", corebosModule: "Adocmaster" }
+const subitoModules = [
+  "MasterAccount",
+  "Accounts",
+  "Task",
+  "SalesOrderMaster",
+  "Cases",
+  "AccountDetails",
+  "Term",
+  "Adocmaster"
 ];
 
-const routes = [
-  { name: "contact/:id", module: "detailview", corebosModule: "Contacts" },
-  { name: "account/:id", module: "detailview", corebosModule: "Accounts" },
-  { name: "account", module: "listview", corebosModule: "Accounts", sidebar: true },
-  { name: "contact", module: "listview", corebosModule: "Contacts", sidebar: true },
-  { name: "task", module: "listview", corebosModule: "Task", sidebar: true }
-];
+const modules = ["Accounts", "Contacts", "Task"];
+
+const routes = modules.reduce((acc, corebosModule) => {
+  const moduleName = corebosModule.toLowerCase();
+
+  return [
+    ...acc,
+    { name: `${moduleName}/:id`, module: "detailview", corebosModule },
+    { name: moduleName, module: "listview", corebosModule, sidebar: true }
+  ];
+}, []);
 
 export default routes;
