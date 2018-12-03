abca065652af7f52a38827f93204c4af02fec4d7
Andi Ndrecka <andi.ndrecka1@gmail.com>
2018-12-03 16:48:03 +0100

    remove corebos client and php utils for corebos

--- packages/shared-utils/index.js
+++ packages/shared-utils/index.js
@@ -1,6 +1,4 @@
 export * from "./src/common";
-export * from "./src/php-helpers";
-export { default as cbClient } from "./src/cbClient";
 export { default as cbMD5 } from "./src/cbMD5";
 export { default as normalize } from "./src/normalize";
 export * from "./src/transform";
--- packages/shared-utils/src/cbClient/index.js
+++
@@ -1,8 +0,0 @@
-import cbClient from "./WSClientp";
-
-export default async () => {
-  const client = new cbClient(GLOBALS.COREBOS_API);
-  await client.doLogin(GLOBALS.COREBOS_USERNAME, GLOBALS.COREBOS_TOKEN);
-
-  return client;
-};
