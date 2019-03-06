844901ecc47259380b977014a2e2b8e39baa2a4d
Besart Marku <b.marku@evolutivo.it>
2019-03-06 11:21:06 +0100

    Add steps

--- packages/feature-form-builder/src/data.js
+++ packages/feature-form-builder/src/data.js
@@ -1,7 +1,7 @@
 const data = {
   steps: [
     {
-      stepname: "Contacts",
+      stepname: "Contacts Steps",
       module: "Contacts",
       stepdesc: "New layout for Assets",
       stepid: 0,
@@ -87,132 +87,234 @@ const data = {
       ]
     },
     {
-      stepname: "Document",
-      stepdesc: "Attach Document",
-      stepid: 2,
-      sequence: "2",
+      stepname: "Accounts Step nr.2",
+      module: "Contacts",
+      stepdesc: "New layout for Assets",
+      stepid: 1,
+      sequence: "1",
       nextaction: "Detail",
       nextsave: "TRUE",
       blocks: [
         {
-          label: "Attachment",
-          sequence: "2",
-          blocktype: "DocumentForm",
+          blockid: 0,
+          label: "Main Data",
+          sequence: "1",
+          blocktype: "Fields",
           fields: [
             {
-              stepname: "Document Attachment",
-              stepdesc: "New layout for Assets",
-              stepid: 1,
+              uitype: 1,
+              fieldid: "f01",
+              blockid: "ba",
+              name: "firstname2",
               sequence: "1",
-              nextaction: "Detail",
-              nextsave: "TRUE",
-              blocks: [
-                {
-                  label: "Document Upload",
-                  sequence: "1",
-                  blocktype: "Fields",
-                  fields: [
-                    {
-                      uitype: 1,
-                      fieldid: "f01",
-                      blockid: "ba",
-                      name: "assetname",
-                      sequence: "1",
-                      label: "Document name"
-                    },
-                    {
-                      uitype: 5,
-                      fieldid: "f01",
-                      blockid: "ba",
-                      name: "assetname2",
-                      sequence: "1",
-                      label: "Upload file"
-                    }
-                  ]
-                }
-              ]
+              label: "Asset Name asdasdasd"
+            },
+
+            {
+              uitype: 1,
+              fieldid: "f03",
+              blockid: "bb",
+              name: "firstname",
+              sequence: "3",
+              label: "Asset Image"
+            },
+
+            {
+              uitype: 1,
+              fieldid: "f02",
+              blockid: "ba",
+              name: "lastname",
+              sequence: "2",
+              label: "Asset Serial Number"
             },
             {
-              stepname: "Document Fields ",
-              stepdesc: "New layout for Assets",
-              stepid: 2,
+              uitype: 1,
+              fieldid: "f02",
+              blockid: "ba",
+              name: "lastname3",
+              sequence: "2",
+              label: "Asset Serial Number"
+            },
+            {
+              uitype: 1,
+              fieldid: "f02",
+              blockid: "ba",
+              name: "lastname4",
+              sequence: "2",
+              label: "Asset Serial Number"
+            }
+          ]
+        },
+        {
+          blockid: 1,
+          label: "Main Data2",
+          sequence: "2",
+          blocktype: "Fields",
+
+          fields: [
+            {
+              uitype: 1,
+              fieldid: 1,
+              blockid: "ba2",
+              name: "Input #1",
               sequence: "1",
-              nextaction: "Detail",
-              nextsave: "TRUE",
-              blocks: [
-                {
-                  label: "Document Fields",
-                  sequence: "1",
-                  blocktype: "Fields",
-                  fields: [
-                    {
-                      uitype: 1,
-                      fieldid: "f01",
-                      blockid: "ba",
-                      name: "assetname",
-                      sequence: "1",
-                      label: "Asset Name"
-                    },
-                    {
-                      uitype: 1,
-                      fieldid: "f02",
-                      blockid: "ba",
-                      name: "serailno",
-                      sequence: "2",
-                      label: "Asset Serial Number"
-                    },
-                    {
-                      uitype: 1,
-                      fieldid: "f03",
-                      blockid: "bb",
-                      name: "cf_999",
-                      sequence: "3",
-                      label: "Asset Image"
-                    }
-                  ]
-                },
-                {
-                  blockid: 1,
-                  label: "Main Data2",
-                  sequence: "2",
-                  blocktype: "RowEdit",
-                  fields: [
-                    {
-                      fieldid: 1,
-                      blockid: "ba",
-                      name: "Input #1",
-                      sequence: "1",
-                      label: "Input #1"
-                    },
-                    {
-                      fieldid: 2,
-                      blockid: "ba",
-                      name: "Input #2",
-                      sequence: "2",
-                      label: "Input #2"
-                    },
-                    {
-                      fieldid: 3,
-                      blockid: "ba",
-                      name: "Input #1",
-                      sequence: "1",
-                      label: "Input #3"
-                    },
-                    {
-                      fieldid: 4,
-                      blockid: "ba",
-                      name: "Input #2",
-                      sequence: "2",
-                      label: "Input #4"
-                    }
-                  ]
-                }
-              ]
+              label: "Input #1"
+            },
+            {
+              uitype: 1,
+              fieldid: 2,
+              blockid: "ba",
+              name: "Input #2",
+              sequence: "2",
+              label: "Input #2"
+            },
+            {
+              uitype: 1,
+              fieldid: 3,
+              blockid: "ba",
+              name: "Input #1",
+              sequence: "1",
+              label: "Input #3"
+            },
+            {
+              uitype: 1,
+              fieldid: 4,
+              blockid: "ba",
+              name: "Input #2",
+              sequence: "2",
+              label: "Input #4"
             }
           ]
         }
       ]
     }
+    // {
+    //   stepname: "Document",
+    //   stepdesc: "Attach Document",
+    //   stepid: 2,
+    //   sequence: "2",
+    //   nextaction: "Detail",
+    //   nextsave: "TRUE",
+    //   blocks: [
+    //     {
+    //       label: "Attachment",
+    //       sequence: "2",
+    //       blocktype: "DocumentForm",
+    //       fields: [
+    //         {
+    //           stepname: "Document Attachment",
+    //           stepdesc: "New layout for Assets",
+    //           stepid: 1,
+    //           sequence: "1",
+    //           nextaction: "Detail",
+    //           nextsave: "TRUE",
+    //           blocks: [
+    //             {
+    //               label: "Document Upload",
+    //               sequence: "1",
+    //               blocktype: "Fields",
+    //               fields: [
+    //                 {
+    //                   uitype: 1,
+    //                   fieldid: "f01",
+    //                   blockid: "ba",
+    //                   name: "assetname",
+    //                   sequence: "1",
+    //                   label: "Document name"
+    //                 },
+    //                 {
+    //                   uitype: 5,
+    //                   fieldid: "f01",
+    //                   blockid: "ba",
+    //                   name: "assetname2",
+    //                   sequence: "1",
+    //                   label: "Upload file"
+    //                 }
+    //               ]
+    //             }
+    //           ]
+    //         },
+    //         {
+    //           stepname: "Document Fields ",
+    //           stepdesc: "New layout for Assets",
+    //           stepid: 2,
+    //           sequence: "1",
+    //           nextaction: "Detail",
+    //           nextsave: "TRUE",
+    //           blocks: [
+    //             {
+    //               label: "Document Fields",
+    //               sequence: "1",
+    //               blocktype: "Fields",
+    //               fields: [
+    //                 {
+    //                   uitype: 1,
+    //                   fieldid: "f01",
+    //                   blockid: "ba",
+    //                   name: "assetname",
+    //                   sequence: "1",
+    //                   label: "Asset Name"
+    //                 },
+    //                 {
+    //                   uitype: 1,
+    //                   fieldid: "f02",
+    //                   blockid: "ba",
+    //                   name: "serailno",
+    //                   sequence: "2",
+    //                   label: "Asset Serial Number"
+    //                 },
+    //                 {
+    //                   uitype: 1,
+    //                   fieldid: "f03",
+    //                   blockid: "bb",
+    //                   name: "cf_999",
+    //                   sequence: "3",
+    //                   label: "Asset Image"
+    //                 }
+    //               ]
+    //             },
+    //             {
+    //               blockid: 1,
+    //               label: "Main Data2",
+    //               sequence: "2",
+    //               blocktype: "RowEdit",
+    //               fields: [
+    //                 {
+    //                   fieldid: 1,
+    //                   blockid: "ba",
+    //                   name: "Input #1",
+    //                   sequence: "1",
+    //                   label: "Input #1"
+    //                 },
+    //                 {
+    //                   fieldid: 2,
+    //                   blockid: "ba",
+    //                   name: "Input #2",
+    //                   sequence: "2",
+    //                   label: "Input #2"
+    //                 },
+    //                 {
+    //                   fieldid: 3,
+    //                   blockid: "ba",
+    //                   name: "Input #1",
+    //                   sequence: "1",
+    //                   label: "Input #3"
+    //                 },
+    //                 {
+    //                   fieldid: 4,
+    //                   blockid: "ba",
+    //                   name: "Input #2",
+    //                   sequence: "2",
+    //                   label: "Input #4"
+    //                 }
+    //               ]
+    //             }
+    //           ]
+    //         }
+    //       ]
+    //     }
+    //   ]
+    // }
   ]
 };
 
--- packages/feature-form-builder/src/view/index.jsx
+++ packages/feature-form-builder/src/view/index.jsx
@@ -3,16 +3,92 @@ import Modular from "modular-redux";
 import { compose } from "redux";
 import { connect } from "react-redux";
 import Form from "./components/FormBuilder";
-import data from "../data";
+import data2 from "../data";
 import NewForm from "./form";
 
-import { mapToDispatch, mapToState } from "shared-utils";
+import { Tabs, TabsPanel, Button } from "@salesforce/design-system-react";
+import update from "immutability-helper";
 
 class FormBuilder extends Component {
-  state = {
-    formData: {}
+  constructor(props) {
+    super(props);
+    this.state = {
+      formData: {},
+      data: data2
+    };
+
+    this.addRow = this.addRow.bind(this);
+  }
+
+  //Used for blocktype='RowEdit' to make it dynamic
+  addRow = () => {
+    let newState = update(this.state, {
+      data: {
+        steps: {
+          [0]: {
+            blocks: {
+              [0]: {
+                fields: {
+                  $push: [
+                    {
+                      uitype: 1,
+                      fieldid: "f011",
+                      blockid: "ba",
+                      name: "firstname",
+                      sequence: "10",
+                      label: "Added"
+                    }
+                  ]
+                }
+              }
+            }
+          }
+        }
+      }
+    });
+    this.setState(newState);
   };
 
+  //Main Render
+  render() {
+    const { steps } = this.state.data;
+    console.log(steps);
+    return (
+      <div>
+        <Tabs id="tabs-example-default">
+          {steps.map(step => (
+            <TabsPanel key={step.stepid} label={step.stepname}>
+              <div className="slds-p-horizontal_xx-large">{this.renderStep(step)}</div>
+            </TabsPanel>
+          ))}
+        </Tabs>
+        {/* <div>{name}</div>
+          <div onClick={() => actions.rename("wow")}>Change Name</div> */}
+      </div>
+    );
+  }
+
+  //Render Steps
+  renderStep = step => {
+    const { modules } = this.props;
+    console.log(step);
+    console.log("----");
+
+    let meta = modules[step.module];
+    return step.blocks
+      .sort((a, b) => {
+        const aSequence = parseInt(a.sequence);
+        const bSequence = parseInt(b.sequence);
+        return aSequence - bSequence;
+      })
+      .map(({ blocktype, ...rest }) => {
+        if (blocktype === "Fields") {
+          return this.renderBlockFields(meta, rest);
+        }
+      });
+  };
+
+  //Render fields of blocktype=='Fields'
   renderBlockFields = (meta, block) => {
     const { formData } = this.state;
     let { fields, blockid, label, sequence } = block;
@@ -29,7 +105,35 @@ class FormBuilder extends Component {
       }))
       .reduce((acc, field) => ({ ...acc, [field.name]: field }), {});
     meta = { ...meta, fields };
+    return (
+      <>
+        <NewForm
+          onFormChange={data => this.setState({ formData: { ...formData, ...data } })}
+          key={blockid}
+          moduleMeta={meta}
+        />
+        <Button onClick={this.addRow} label="Add new" variant="brand" />
+      </>
+    );
+  };
 
+  //Render fields of blocktype=='Fields'
+  renderBlockRowEdit = (meta, block) => {
+    const { formData } = this.state;
+    let { fields, blockid, label, sequence } = block;
+    fields = fields
+      .map(field => ({
+        displaytype: "1",
+        ...meta.fields[field.name],
+        ...field,
+        block: {
+          blockid,
+          blocksequence: sequence,
+          blockname: label
+        }
+      }))
+      .reduce((acc, field) => ({ ...acc, [field.name]: field }), {});
+    meta = { ...meta, fields };
     return (
       <NewForm
         onFormChange={data => this.setState({ formData: { ...formData, ...data } })}
@@ -38,35 +142,6 @@ class FormBuilder extends Component {
       />
     );
   };
-
-  renderStep = step => {
-    const { modules } = this.props;
-    let meta = modules[step.module];
-
-    return step.blocks
-      .sort((a, b) => {
-        const aSequence = parseInt(a.sequence);
-        const bSequence = parseInt(b.sequence);
-        return aSequence - bSequence;
-      })
-      .map(({ blocktype, ...rest }) => {
-        if (blocktype === "Fields") {
-          return this.renderBlockFields(meta, rest);
-        }
-      });
-  };
-
-  render() {
-    const step = data.steps[0];
-
-    return (
-      <div>
-        {this.renderStep(step)}
-        {/* <div>{name}</div>
-        <div onClick={() => actions.rename("wow")}>Change Name</div> */}
-      </div>
-    );
-  }
 }
 
 // const mapStateToProps = (state, { Module }) =>
