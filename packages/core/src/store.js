bac69068fda5664be796a7c360f1de178e426e4a
Altin <softize@gmail.com>
2019-05-24 14:45:42 +0200

    fix deprecation warning for History middleware

--- packages/core/src/store.js
+++ packages/core/src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
-import createHistory from "history/createBrowserHistory";
+import { createBrowserHistory as createHistory } from "history";
 import { routerMiddleware } from "react-router-redux";
 import { createEpicMiddleware } from "redux-observable";
 import { Repo } from "shared-repo";
